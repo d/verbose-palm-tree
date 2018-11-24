b83f13264f66447f197ee6fbbf51eb3143279af3
Jesse Zhang <sbjesse@gmail.com>
2018-11-24 16:09:27 -0700

    initial support for incremental rebuild
    just call /path/to/pg-bear-rebuild.bash -C /path/to/postgres

--- pg-bear-rebuild.bash
+++ pg-bear-rebuild.bash
@@ -1,7 +1,39 @@
 #!/bin/bash
 
+set -e -u -o pipefail
+
+rebuild() {
+	local directory=$1
+
+	git -C "${directory}" pull --ff-only
+	gmake -s clean -C "${directory}/src/interfaces"
+	bear --cdb "${directory}/compile_commands.json" --append gmake -s -j"${NCPU}" -l"${MAXLOAD}" --output-sync -C "${directory}"
+}
+
 _main() {
-	true
+	local directory
+
+	while getopts C: opt; do
+		case $opt in
+			C)
+				directory=$OPTARG
+				;;
+			*)
+				printf >&2 'Unexpected argument\n'
+				exit 1
+				;;
+		esac
+	done
+
+	if [ "${directory:+x}" != x ]; then
+		echo >&2 You need to specify a directory with -C
+
+		false
+	fi
+
+	NCPU=$(sysctl -n hw.ncpu)
+	MAXLOAD=$(( 2 * NCPU ))
+	rebuild "${directory}"
 }
 
 
